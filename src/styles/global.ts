85b86dfbbfb59a59799b26a8e63bcb7399bce722
Victor Oliveira Santos <victor_oliveirasantos@hotmail.com>
2022-07-21 17:19:20 -0300

    Add font google api

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -32,11 +32,18 @@ export const GlobalStyle = createGlobalStyle`
         }
     }
 
-    // REM = 1rem = 16px
-
     body {
         background: var(--background);
         -webkit-font-smoothing: antialiased;
+    }
+
+    body, input, textarea, button {
+        font-family: 'Poppins', sans-serif;
+        font-weight: 400;
+    }
+
+    h1, h2, h3, h4, h5, h6, strong {
+        font-weight: 600;
     }
 
     button {
