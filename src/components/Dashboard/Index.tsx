4b24caabc65f1c48e0f586a472634fd6b51f19a3
Victor Oliveira Santos <victor_oliveirasantos@hotmail.com>
2022-07-25 18:01:55 -0300

    Add Transaction to Dashboard component

--- src/components/Dashboard/Index.tsx
+++ src/components/Dashboard/Index.tsx
@@ -2,11 +2,13 @@ import { Summary } from "../Summary";
 
 import { Container } from "./styles";
 
+import { TransactionTable } from "../TransactionsTable";
 
 export function Dashboard () {
     return (
         <Container>
             <Summary />
+            <TransactionTable />
         </Container>
     );
 }
